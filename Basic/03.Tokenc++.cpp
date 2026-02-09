0b57b556a46ca0af268be024d2a345fe24abfe73
Daleepswami <daleepswami22170@gmail.com>
2026-02-10 01:37:20 +0530

    Create 03.Tokenc++.cpp

---
+++ Basic/03.Tokenc++.cpp
@@ -0,0 +1,20 @@
+What is a Token in C++
+Definition:=>>A token is the smallest meaningful unit of a C++ program.
+types of token::=>
+     01. keywords:--Reserved words with predefined meaning in C++.
+       ex.::=> int , class ,if ,else ,try ,catch etc 
+     02.Identifiers:-Names given to variables, functions, or arrays.
+       ex:->int marks;
+            float total_sum;
+            //marks, total_sum → identifiers
+     03. Constants / Literals:--Fixed values that do not change during program execution.
+       ex:-int x = 10;
+           char ch = 'A';
+           10, 'A' → constants
+     04.Operators:->>Symbols used to perform operations.
+       ex:- int a = 5 + 3;
+            a++;
+     05. Special Symbols:--Characters used to structure the program.
+       ex:-int x = 10;
+            cout << x;
+            ;, { }, << → special symbols
