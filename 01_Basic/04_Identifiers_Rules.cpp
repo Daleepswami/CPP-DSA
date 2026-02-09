c2e80436e455b8e2c08e8f69a19fabddf0c1ac30
Daleepswami <daleepswami22170@gmail.com>
2026-02-10 03:41:03 +0530

    Add files via upload

---
+++ 01_Basic/04_Identifiers_Rules.cpp
@@ -0,0 +1,27 @@
+What is an Identifier ?
+==>>  An identifier is the name given to a variable, function, or array.
+
+Rules for Identifiers in C++:-->
+  01️. Must start with a letter or underscore:-
+      int age;        // valid
+      int _count;    // valid
+      int 2num;      // invalid
+  02. Can contain letters, digits, and underscore only:-->
+      int total_marks;   // valid
+      int total$marks;  // invalid
+  03. Cannot use keywords as identifiers:-->
+      int int;     // invalid
+      int number; // valid
+  04. No spaces allowed:->>
+      int total marks;   // invalid
+      int totalMarks;   // valid
+  05.Case-sensitive:==>
+      int value;
+      int Value;   // different identifier
+
+      
+
+    
+
+
+     
---
+++ 01_Basic/05_Keywords.cpp
@@ -0,0 +1,67 @@
+What is a Keyword ?
+   Keywords are reserved words in C++.
+   They have a predefined meaning and cannot be used as identifiers.
+   Keyword = reserved word, fixed meaning, cannot be changed 
+All C++ Keywords (Complete List)::==> 
+
+	
+	Basic / Data related::=>
+	        int     float     double     char     bool     void
+                short   long      signed     unsigned
+
+
+        Control statements::=>> 
+	        if      else      switch     case      default
+                for     while     do
+                break   continue  goto       return
+
+	
+	Storage class::=>>
+                auto    register  static     extern    mutable
+
+
+        Object-Oriented Programming::==>>
+	        class   struct    union      enum
+                public  private   protected
+                virtual friend    this       new        delete
+
+	
+	Function / Exception handling::=>>
+	        try     catch     throw      noexcept
+
+	
+	Type-related / Advanced::=>>
+	        const       volatile     constexpr
+                sizeof      typedef      typeid
+                inline      explicit
+
+
+        Casting keywords::=>>
+                static_cast
+                dynamic_cast
+                const_cast
+                reinterpret_cast
+
+
+        Boolean / Logic::=>>
+	        true    false
+
+	
+	Concurrency (C++11+)::=>
+	        thread_local
+
+	
+	Misc / Rare but keywords::=>>
+                operator
+                asm
+                nullptr
+
+
+
+
+
+
+	
+
+
+
