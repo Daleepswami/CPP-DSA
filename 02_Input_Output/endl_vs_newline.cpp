e359e50e559e7629c645c4a202ebc8e5de2da4d5
Daleepswami <daleepswami22170@gmail.com>
2026-02-13 23:53:55 +0530

    Update endl_vs_newline.cpp

--- 02_Input_Output/endl_vs_newline.cpp
+++ 02_Input_Output/endl_vs_newline.cpp
@@ -1,1 +1,27 @@
+// ========================================
+// endl vs \n in C++
+// ========================================
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello World" << endl;
+    cout << "Welcome to C++" << endl;
+
+    cout << "Hello World\n";
+    cout << "Welcome to C++\n";
+
+    return 0;
+}
+
+
+| endl                              | \n            |
+| --------------------------------- | ------------- |
+| New line + buffer flush           | Only new line |
+| Slower                            | Faster        |
+| Used when immediate output needed | Used normally |
+|  low                              | fast          |
+
+  
