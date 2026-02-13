4626dada1922ba0e7a0bda8bfd467eff7d94be89
Daleepswami <daleepswami22170@gmail.com>
2026-02-13 22:03:52 +0530

    Update size_of_datatypes.cpp

--- 03_Data_Types/size_of_datatypes.cpp
+++ 03_Data_Types/size_of_datatypes.cpp
@@ -1,1 +1,21 @@
+// ========================================
+// SIZEOF OPERATOR IN C++
+// ========================================
 
+// sizeof operator is used to find the size
+// (in bytes) of a data type or variable.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    cout << "Size of bool: " << sizeof(bool) << " bytes" << endl;
+
+    return 0;
+}
