2b2969437f6f0bad0001ae7afe2d038d410e4df9
Daleepswami <daleepswami22170@gmail.com>
2026-02-13 19:23:17 +0530

    Update 02.Structure_destribe_c++.cpp

--- 01_Basic/02.Structure_destribe_c++.cpp
+++ 01_Basic/02.Structure_destribe_c++.cpp
@@ -1,1 +1,31 @@
+// basic program  in C++
+#include <iostream>
+using namespace std;
+int main(){
+ cout<< "hello";
+}
 
+01.  #include <iostream>
+     # → Preprocessor directive symbol
+     include → Keyword used to include a library
+     <iostream> → Standard library for input and output
+     syntax:->  #include<library_name>
+02.  using namespace std;
+     using → Keyword
+     namespace → Used to group related code
+     std → Standard namespace in C++
+03. int main() 
+    main() → Main function (starting point of program)
+    int → Return type (it returns an integer value)
+
+04.  { } (Curly Braces)
+     All program logic is written inside these braces.
+    {
+        cout << "Hello";
+     }
+
+05. return 0;
+    return → Keyword
+    0 → Means program executed successfully
+
+     
